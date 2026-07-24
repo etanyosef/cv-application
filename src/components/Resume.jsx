0a334bb6edd79aceb5bf9d66caeda88a9bff2304
Ethan <ethancredo.ascc@outlook.com>
2026-07-24 16:25:13 +0800

    create default resume data

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,8 +1,12 @@
-export default function Resume() {
+export default function Resume({personalDetails: {fullName, email, phoneNumber, address}}) {
     return (
         <div className="resume">
             <div className="resume-header">
                 <h2>Resume Header</h2>
+                <p>Full name: {fullName}</p>
+                <p>Email: {email}</p>
+                <p>Phone: {phoneNumber}</p>
+                <p>address: {address}</p>
             </div>
             <div className="resume-content">
                 <h2>Resume Content</h2>
---
+++ src/defaultData.js
@@ -0,0 +1,8 @@
+export const defaultData = {
+    personalDetails: {
+        fullName: 'Sagi Cat',
+        email: 'sagi@cat.com',
+        phoneNumber: '09123456789',
+        address: 'Miputak, Dipolog City'
+    }
+}
