40d12f86786232356ddf5f5ec77b00cca324765c
Ethan <ethancredo.ascc@outlook.com>
2026-07-24 16:46:11 +0800

    pass props to PersonalDetails and Resume

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react"
 import Education from "./Education"
 import Experience from "./Experience"
 import Footer from "./Footer"
 import PersonalDetails from "./PersonalDetails"
 import Resume from "./Resume"
+import { defaultData } from "../defaultData"
 
 export default function App() {
     const [personalDetails, setPersonalDetails] = useState(defaultData.personalDetails)
@@ -19,12 +21,19 @@ export default function App() {
     return (
         <>
             <main>
-                <div className="sidebar">
-                    <PersonalDetails />
-                    <Education />
-                    <Experience />
+                <div className="container">
+                    <div className="sidebar">
+                        <PersonalDetails 
+                            data={personalDetails}
+                            onChange={handleOnChange}
+                        />
+                        <Education />
+                        <Experience />
+                    </div>
+                    <Resume
+                        personalDetails={personalDetails}
+                    />
                 </div>
-                <Resume />
             </main>
             <Footer />
         </>
