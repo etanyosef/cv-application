a38760f8bf53e88a7274872fee52ec9a26a2c04e
Ethan Joseph <ethancredo.ascc@outlook.com>
2026-07-26 00:02:10 +0800

    add icons

--- src/components/sidebar/forms/EducationForm.jsx
+++ src/components/sidebar/forms/EducationForm.jsx
@@ -9,7 +9,9 @@ export default function EducationForm({
         <div className="education-details">
             <div className="title">
                 <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>school-outline</title><path d="M12 3L1 9L5 11.18V17.18L12 21L19 17.18V11.18L21 10.09V17H23V9L12 3M18.82 9L12 12.72L5.18 9L12 5.28L18.82 9M17 16L12 18.72L7 16V12.27L12 15L17 12.27V16Z" /></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path d="M12 3L1 9L5 11.18V17.18L12 21L19 17.18V11.18L21 10.09V17H23V9L12 3M18.82 9L12 12.72L5.18 9L12 5.28L18.82 9M17 16L12 18.72L7 16V12.27L12 15L17 12.27V16Z" />
+                    </svg>
                     <h2>Education</h2>
                 </span>
                 
@@ -64,8 +66,8 @@ export default function EducationForm({
                         required
                     />
                 </div>
-                <button type="submit">
-                    Add
+                <button className="form-button" type="submit">
+                    + Add
                 </button>
             </form>
         </div>
