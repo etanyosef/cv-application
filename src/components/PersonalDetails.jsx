9bb9cdff3f1f8c775d9615059a37d31128d35311
Ethan <ethancredo.ascc@outlook.com>
2026-07-25 11:49:35 +0800

    add data- attribute

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -12,7 +12,7 @@ export default function PersonalDetails({
                 </span>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>chevron-down</title><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
             </div>
-            <form>
+            <form data-section="personalDetails">
                 <div className="form-input">
                     <label htmlFor="full-name">Full Name</label>
                     <input 
