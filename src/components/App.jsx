5db94b24e7c42511d185b5c4ad1a1326d8b9a9d0
Ethan Joseph <ethancredo.ascc@outlook.com>
2026-07-25 22:58:31 +0800

    create add education handler

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react"
-import EducationForm from "./EducationForm"
-import Experience from "./Experience"
+import { 
+    defaultData,
+    educationDetails,
+} from "../data"
+
 import Footer from "./Footer"
-import PersonalDetails from "./PersonalDetails"
 import Resume from "./Resume"
-import { defaultData } from "../defaultData"
+import Sidebar from "./sidebar/Sidebar"
 
 export default function App() {
     const [personalDetails, setPersonalDetails] = useState(defaultData.personalDetails)
-    const [educationDetails, setEducationDetails] = useState(defaultData.educationDetails);
-
-    console.log(educationDetails)
+    const [education, setEducation] = useState(educationDetails)
 
     function handlePersonalDetailsInput(e) {
         const {key} = e.target.dataset
@@ -28,25 +28,67 @@ export default function App() {
         })
     }
 
+    function addEducation() {
+        setEducation((prevEducation) => [
+            ...prevEducation,
+            {
+                id: crypto.randomUUID(),
+                school: '',
+                degree: '',
+                location: '',
+                startDate: '',
+                endDate: '',
+            },
+        ])
+    }
+
+    function educationOnChange(e, id) {
+        const {name} = e.target.name
+        setEducation((prevEducation) => prevEducation.map((educ) => {
+            educ.id === id ? {...educ, [name]: e.target.value} : educ
+        }))
+    }
+
+    function saveEducation(e, id) {
+        const school = e.get('school')
+        const degree = e.get('degree')
+        const location = e.get('location')
+        const startDate = e.get('startDate')
+        const endDate = e.get('endDate')
+
+        setEducation(prevEducation => ([
+                ...prevEducation,
+                {
+                    id: crypto.randomUUID(),
+                    school: school,
+                    degree: degree,
+                    location: location,
+                    startDate: startDate,
+                    endDate: endDate,
+                },
+        ]))
+    }
+
     return (
         <>
             <main>
                 <div className="container">
-                    <div className="sidebar">
-                        <PersonalDetails 
-                            data={personalDetails}
-                            onChange={handlePersonalDetailsInput}
-                        />
-                        <EducationForm
-                            data={educationDetails}
-                            onChange={handleEducationDetailsInput}
-                        />
-                        <Experience />
-                    </div>
-                    <Resume
-                        personalDetails={personalDetails}
-                        educationDetails={educationDetails}
+                    <Sidebar 
+                        personalDetails={personalDetails} 
+                        education={education}
+                        educationOnChange={educationOnChange}
+                        addEducation={addEducation}
+                        saveEducation={saveEducation}
+                        onChange={handlePersonalDetailsInput}
                     />
+
+                    <div className="preview">
+                        <Resume
+                            personalDetails={personalDetails}
+                            education={education}
+                        />
+                    </div>
+
                 </div>
             </main>
             <Footer />
