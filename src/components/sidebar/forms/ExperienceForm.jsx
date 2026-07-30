352d53929e0c2b19116d70b0d39c5cc62b8c99a5
Ethan <ethancredo.ascc@outlook.com>
2026-07-30 14:41:40 +0800

    display reponsilibilies in sidebar

--- src/components/sidebar/forms/ExperienceForm.jsx
+++ src/components/sidebar/forms/ExperienceForm.jsx
@@ -2,7 +2,26 @@ export default function ExperienceForm({
     experience,
     isActive,
     experienceOnChange,
+    responsibilityOnChange,
+    addResponsibility,
 }) {
+
+    const responsibilitiesList = experience.responsibilities.map(resp => (
+        <div key={resp.id}>
+            <div className="side-item">
+                <button>
+                    <h4>{resp.title}</h4>            
+                </button>
+                <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <title>Delete</title>
+                        <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+    ))
+
     return (
         <>
             {isActive ? (
@@ -62,16 +81,54 @@ export default function ExperienceForm({
                         </div>
                         <div className="form-input">
                             <label htmlFor="responsibilities">Responsibilities</label>
-                            <textarea 
+                            {/* <textarea 
                                 id="responsibilities"
                                 data-key="responsibilities"
                                 value={experience.responsibilities}
                                 onChange={e => experienceOnChange(e, experience.id)}
-                            ></textarea>
+                            ></textarea> */}
+                            {responsibilitiesList.length > 0 ? 
+                            <ul>
+                                {responsibilitiesList}
+                            </ul>
+                            : null }
                         </div>
                     </form>
+                    <button 
+                        className="form-button"
+                        onClick={() => addResponsibility(experience.id)}
+                    >+ Add Responsibility</button>
                 </div>
             ) : null}
+            <br />
+            <hr />
         </>
+    )
+}
+
+function responsibilitiesForm(responsibility, responsibilityOnChange) {
+    return (
+        <form>
+            <h3>Responsibility</h3>
+            <div className="form-input">
+                <label htmlFor="title">Title</label>
+                <input 
+                    type="text" 
+                    id="title" 
+                    data-key="title" 
+                    value={responsibility.title}
+                    onChange={responsibilityOnChange}
+                />
+            </div>
+            <div className="form-input">
+                <label htmlFor="description">Description</label>
+                <textarea 
+                    id="description"
+                    data-key="description"
+                    value={responsibility.description}
+                    onChange={responsibilityOnChange}
+                ></textarea>
+            </div>
+        </form>
     )
 }
