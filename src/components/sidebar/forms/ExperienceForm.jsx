4c4654cab06ac44ff499123c1d3306b9da35c198
Ethan <ethancredo.ascc@outlook.com>
2026-07-31 16:35:00 +0800

    add deleteResponsibility function to button

--- src/components/sidebar/forms/ExperienceForm.jsx
+++ src/components/sidebar/forms/ExperienceForm.jsx
@@ -4,6 +4,7 @@ export default function ExperienceForm({
     experienceOnChange,
     responsibilityOnChange,
     addResponsibility,
+    deleteResponsibility
 }) {
 
     const responsibilitiesList = experience.responsibilities.map(resp => (
@@ -17,7 +18,7 @@ export default function ExperienceForm({
                     value={resp.text}
                     onChange={e => responsibilityOnChange(e, experience.id, resp.id)}
                 ></textarea>
-                <button>
+                <button onClick={() => deleteResponsibility(experience.id, resp.id)}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <title>Delete</title>
                         <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
